6a5bb7096d204ce0be4273529375332af5d2a242
Dodecahedrane <oliver.guy-1@students.plymouth.ac.uk>
2021-10-07 14:50:20 +0100

    implmeneted and completed

---
+++ src/counter.java
@@ -0,0 +1,14 @@
+public class counter {
+    public int MAX = 100;
+    public int value;
+
+    public void increment(){
+        if(value<=MAX){
+            value = value + 1;
+        }
+    }
+
+    public void printValue(){
+        System.out.println(value);
+    }
+}
