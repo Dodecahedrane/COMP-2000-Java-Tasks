d8a9c359f9051edc62dfc4d0c39dad5e1e61078d
Dodecahedrane <oliver.guy-1@students.plymouth.ac.uk>
2021-10-07 14:47:03 +0100

    implmented class diagram

--- src/stopwatch.java
+++ src/stopwatch.java
@@ -1,2 +1,12 @@
 public class stopwatch {
+    public int MAX = 100;
+    public int value;
+
+    public void increment(){
+
+    }
+
+    public void printValue(){
+        System.out.println(value);
+    }
 }
