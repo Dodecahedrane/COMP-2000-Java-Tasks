07fb78db590434d9e71e3d1ea985d3bd9ae4e4cf
Dodecahedrane <oliver.guy-1@students.plymouth.ac.uk>
2021-10-07 14:50:05 +0100

    initial commit

--- src/stopwatch.java
+++ src/stopwatch.java
@@ -1,12 +1,5 @@
 public class stopwatch {
-    public int MAX = 100;
-    public int value;
 
-    public void increment(){
 
-    }
 
-    public void printValue(){
-        System.out.println(value);
-    }
 }
