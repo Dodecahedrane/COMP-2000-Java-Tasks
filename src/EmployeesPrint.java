184ab74d5a02b6a30faecd6a365300c7c3747043
Dodecahedrane <oliver.guy-1@students.plymouth.ac.uk>
2021-10-07 13:51:28 +0100

    Task 2 Completed

--- src/EmployeesPrint.java
+++ src/EmployeesPrint.java
@@ -1,11 +1,24 @@
 public class EmployeesPrint {
     static String[] names = {"Dave","Steve","Bob","Jess"};
-    static int[] ages = {23,42,99,34};
+    static int[] ages = {23,52,99,34};
 
-
-    public static void main(String []args){
+    public static void PrintAllEmployees(){
         for(int i = 0; i < names.length; i++){
             System.out.print(names[i] + " " + ages[i] + "\n");
         }
+    }
+
+    public static void PrintAllEmployeesOver50(){
+        for(int i = 0; i < names.length; i++){
+            if(ages[i]>=50){
+                System.out.print(names[i] + " " + ages[i] + "\n");
+            }
+        }
+    }
+
+    public static void main(String []args){
+        PrintAllEmployees();
+        System.out.println("");
+        PrintAllEmployeesOver50();
     }
 }
