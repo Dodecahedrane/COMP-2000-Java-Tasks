234769387dfaee792b7aac9cc272bdf96c5d11ea
Dodecahedrane <oliver.guy-1@students.plymouth.ac.uk>
2021-10-07 13:57:10 +0100

    Task 2 Completed

--- src/car.java
+++ src/car.java
@@ -1,2 +1,17 @@
 public class car {
+    
+    static String model;
+    static int price;
+
+    public void start(){
+
+    }
+
+    public void stop(){
+
+    }
+
+    public void go(){
+
+    }
 }
