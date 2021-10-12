d7e40c631be907891676703335201efd8e9effd1
Dodecahedrane <oliver.guy-1@students.plymouth.ac.uk>
2021-10-12 11:43:44 +0100

    comment about abstract classes added

--- src/inheritanceTest.java
+++ src/inheritanceTest.java
@@ -4,6 +4,13 @@ public class inheritanceTest {
     }
 }
 
+//You can make abstract classes where you dont have to implement any of the methods
+
+//public abstract class inheritancetest {
+//  public void Text();
+//}
+
+
 class specificText extends inheritanceTest{
     public void Text(){
         System.out.println("specific text");
