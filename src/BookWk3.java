b66296a8fff41515bc8cd8c0001bdd698f785ed1
Dodecahedrane <oliver.guy-1@students.plymouth.ac.uk>
2021-10-13 12:09:59 +0100

    Partially implemented

--- src/BookWk3.java
+++ src/BookWk3.java
@@ -1,2 +1,18 @@
 public class BookWk3 {
+    private String title;
+    private String author;
+    private String year;
+
+    public void printTitle(){
+        System.out.println();
+    }
+
+    public void printAuthor(){
+        System.out.println();
+    }
+
+    public void printYear(){
+
+        System.out.println();
+    }
 }
