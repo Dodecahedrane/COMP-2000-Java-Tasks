4c4919d50fac19f0a33268bba1d108ad801da7dc
Dodecahedrane <oliver.guy-1@students.plymouth.ac.uk>
2021-10-14 09:50:55 +0100

    Updated comment on abstract classes so is now correct

--- src/inheritanceTest.java
+++ src/inheritanceTest.java
@@ -7,7 +7,7 @@ public class inheritanceTest {
 //You can make abstract classes where you dont have to implement any of the methods
 
 //public abstract class inheritanceTest {
-//  public void Text();
+//  public abstract void Text();
 //}
 
 
