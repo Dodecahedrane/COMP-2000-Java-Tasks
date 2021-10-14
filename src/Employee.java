9a98d1372ac0bea2b6099ad69ae7fbd4989b6802
Dodecahedrane <oliver.guy-1@students.plymouth.ac.uk>
2021-10-14 09:55:18 +0100

    Completed

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,33 @@
-public abstract class Employee {
+abstract class Employee {
     public abstract void job();
+    public abstract void workEnviroment();
+    public abstract void skills();
+}
+
+class Lecturer extends Employee{
+    public void job(){
+        System.out.println("Lecturer");
+    }
+
+    public void workEnviroment(){
+        System.out.println("University");
+    }
+
+    public void skills(){
+        System.out.println("Teaching");
+    }
+}
+
+class Engineer extends Employee{
+    public void job(){
+        System.out.println("Engineer");
+    }
+
+    public void workEnviroment(){
+        System.out.println("Office");
+    }
+
+    public void skills(){
+        System.out.println("CAD");
+    }
 }
