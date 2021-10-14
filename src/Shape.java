333114474c31551fb3673993b68c46d5e567bc14
Dodecahedrane <oliver.guy-1@students.plymouth.ac.uk>
2021-10-14 10:03:24 +0100

    Compeleted

--- src/Shape.java
+++ src/Shape.java
@@ -3,9 +3,35 @@ interface Shape {
     public int howManySides();
 }
 
+class Square implements Shape{
+    public void draw(){
+        System.out.println("□");
+    }
+
+    public int howManySides(){
+        return 4;
+    }
+}
+
+class Rectangle implements Shape{
+    public void draw(){
+        System.out.println("▭");
+    }
+
+    public int howManySides(){
+        return 4;
+    }
+}
+
 
 class MainShape{
     public static void main(String[] args){
+        Square s = new Square();
+        Rectangle r = new Rectangle();
 
+        s.draw();
+        System.out.println(s.howManySides());
+        r.draw();
+        System.out.println(r.howManySides());
     }
 }
